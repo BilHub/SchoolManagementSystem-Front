9f21fb95f12bafe2a575b538fce4fb58d3b1d520
BilHub <billel@myem.fr>
2023-04-08 15:19:09 +0100

    fixing delete cycle

--- src/components/accountPanel/courses/section/TableCycle.js
+++ src/components/accountPanel/courses/section/TableCycle.js
@@ -11,8 +11,15 @@ const TableCycle = memo(({ cycleList, refetch, setCycleList }) => {
   const navigate = useNavigate();
   const location = useLocation();
   const deleteCycle = async (id) => {
+    const token = JSON.parse(localStorage.getItem("token"));
     await axios
-      .delete(`http://127.0.0.1:8000/api/v1/cycle/${id}/`)
+      .delete(`http://127.0.0.1:8000/api/v1/cycle/${id}/`, {
+        headers: {
+          "Content-Type": "application/json",
+          accept: "application/json",
+          Authorization: "JWT " + token,
+        },
+      })
       .then((response) => {
         setCycleList(null);
         refetch();
