c0c46e201850607a53ac82f1428a1f140b2d0a90
BilHub <billel@myem.fr>
2023-05-25 01:35:37 +0200

    changing baseURL

--- src/utils/backend.instance.js
+++ src/utils/backend.instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 export const api = axios.create({
-  // baseURL: "http://127.0.0.1:8000/", # baseURL for dev
-  baseURL: "https://api-school.code2bind.com/",
+  // baseURL: "https://api-school.code2bind.com/",
+  // baseURL: "http://127.0.0.1:8000/",
+  baseURL: "http://164.90.188.42/",
   headers: {
     "Content-Type": "application/json",
   },
