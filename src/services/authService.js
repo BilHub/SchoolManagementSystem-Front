158d4bec6c33001479932511f19ab496bf0a1191
BilHub <billel@myem.fr>
2023-05-25 12:00:09 +0200

    login.js

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import {api, setAuthToken} from "../utils/backend.instance";
+import { api, setAuthToken } from "../utils/backend.instance";
 const registerAPI = (body) => {
   return api.post("api/v1/school/create_school_account/", body, {
     headers: {
@@ -9,12 +9,26 @@ const registerAPI = (body) => {
   });
 };
 
+// const loginAPI = (body) => {
+//   return api
+//     .post("api/v1/auth/login/", body)
+//     .then((res) => {
+//       if (res.data.access) {
+//         setAuthToken(res.data.access)
+//         localStorage.setItem("token", JSON.stringify(res.data.access));
+//         localStorage.setItem("user", JSON.stringify(res.data.user));
+//         console.log("response from loginAPI:", res);
+//       }
+//       return res.data;
+//     })
+//     .catch((err) => console.log(err));
+// };
 const loginAPI = (body) => {
-  return api
-    .post("api/v1/auth/login/", body)
+  return axios
+    .post("http://164.90.188.42/api/v1/auth/login/", body)
     .then((res) => {
       if (res.data.access) {
-        setAuthToken(res.data.access)
+        setAuthToken(res.data.access);
         localStorage.setItem("token", JSON.stringify(res.data.access));
         localStorage.setItem("user", JSON.stringify(res.data.user));
         console.log("response from loginAPI:", res);
