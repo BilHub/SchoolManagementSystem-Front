445fcde47e8f21a05b735571197f8b4065695eb8
BilHub <billel@myem.fr>
2023-05-25 17:52:48 +0200

    backend instance

--- src/services/authService.js
+++ src/services/authService.js
@@ -9,23 +9,9 @@ const registerAPI = (body) => {
   });
 };
 
-// const loginAPI = (body) => {
-//   return api
-//     .post("api/v1/auth/login/", body)
-//     .then((res) => {
-//       if (res.data.access) {
-//         setAuthToken(res.data.access)
-//         localStorage.setItem("token", JSON.stringify(res.data.access));
-//         localStorage.setItem("user", JSON.stringify(res.data.user));
-//         console.log("response from loginAPI:", res);
-//       }
-//       return res.data;
-//     })
-//     .catch((err) => console.log(err));
-// };
 const loginAPI = (body) => {
-  return axios
-    .post("http://164.90.188.42/api/v1/auth/login/", body)
+  return api
+    .post("api/v1/auth/login/", body)
     .then((res) => {
       if (res.data.access) {
         setAuthToken(res.data.access);
--- src/utils/backend.instance.js
+++ src/utils/backend.instance.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://127.0.0.1:8000/",
-  baseURL: "http://164.90.188.42/",
+  // baseURL: "http://164.90.188.42/",
+  baseURL: "http://allin1school.com/",
   // baseURL: "https://api-school.code2bind.com/",
   headers: {
     "Content-Type": "application/json",
