80ced35ab04709cd859894b6eb8c760e5c1d545a
BilHub <billel@myem.fr>
2023-06-23 12:01:30 +0200

    changing backend instance

--- src/utils/backend.instance.js
+++ src/utils/backend.instance.js
@@ -2,7 +2,7 @@ import axios from "axios";
 export const api = axios.create({
   // baseURL: "http://127.0.0.1:8000/",
   // baseURL: "http://164.90.188.42/",
-  baseURL: "https://api.allin1school.pro/",
+  baseURL: "https://13.53.174.222/",
   // baseURL: "https://api-school.code2bind.com/",
   headers: {
     "Content-Type": "application/json",
